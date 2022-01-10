3a2e3b760f81b137dc9ce05eb83b8b05e90ad9ac
Zomer <zomerzxc.19@gmail.com>
2022-01-10 14:04:32 +0800

    add create source

---
+++ src/payment/source/create-source.ts
@@ -0,0 +1,84 @@
+import api from "../../utils/api-base";
+import {
+  CreateSourceResponse,
+  SourceType,
+  RedirectType,
+  CurrencyType,
+  BillingProps,
+} from "../types";
+
+const WEB_URL = "http://localhost:3000";
+
+export interface CreateSourceProps {
+  amount: number;
+  redirect: RedirectType;
+  type: SourceType;
+  currency: CurrencyType;
+  billing?: BillingProps;
+}
+
+const defaultProps: CreateSourceProps = {
+  amount: 0,
+  redirect: {
+    success: `${WEB_URL}/payments/success`,
+    failed: `${WEB_URL}/payments/error`,
+  },
+  type: "gcash",
+  currency: "PHP",
+};
+
+/**
+ * @module createSource
+ * @property {number} amount - amount of the payment source in cents (PHP100 = 100000).
+ * @property {RedirectType} redirect - redirect url for success and failed payment.
+ * @property {SourceType} type - type of the payment source, it's either gcash | grab_pay.
+ * @property {CurrencyType} currency - currency of the payment source, defaults to PHP.
+ * @property {BillingProps} billing - billing information of the payment source.
+ * @returns {CreateSourceResponse} - The response of the create source request.
+ *
+ * @example
+ * ```js
+ * import PaymongoClient from "paymongo.js";
+ *
+ * const main = async () => {
+ *  const client = PaymongoClient("pk_test_key");
+ *  const data = await client.createSource({
+ *    amount: 10000,
+ *    redirect: {
+ *      success: `http://localhost:3000/payments/success`,
+ *      failed: `http://localhost:3000/payments/error`,
+ *    },
+ *    type: "gcash",
+ *    currency: "PHP",
+ *  });
+ *
+ *  return data
+ * }
+ * ```
+ */
+export const createSource = async ({
+  amount = defaultProps.amount,
+  redirect = defaultProps.redirect,
+  type = defaultProps.type,
+  currency = defaultProps.currency,
+  billing,
+}: CreateSourceProps): Promise<CreateSourceResponse> => {
+  const data: any = {
+    attributes: {
+      amount,
+      redirect,
+      type,
+      currency,
+    },
+  };
+
+  if (billing) data.attributes.billing = billing;
+
+  try {
+    const response = await api.post<CreateSourceResponse>("/sources", data);
+    return response.data;
+  } catch (err) {
+    const error: any = err;
+    return error.response.data;
+  }
+};
--- src/payment/types.ts
+++ src/payment/types.ts
@@ -30,5 +30,47 @@ export interface PaymentIntentResponse {
   };
 }
 
+export interface CreateSourceResponse {
+  data: {
+    id: string;
+    type: string;
+    attributes: {
+      amount: number;
+      billing?: any;
+      currency: CurrencyType;
+      description?: any;
+      livemode: boolean;
+      redirect: RedirectType;
+      statement_descriptor?: any;
+      status: string;
+      type: SourceType;
+      created_at: number;
+      updated_at: number;
+    };
+  };
+}
+
+export interface BillingProps {
+  name?: string;
+  phone?: string;
+  email?: string;
+  address?: AddressType;
+}
+
+export type AddressType = {
+  line1?: string;
+  line2?: string;
+  city?: string;
+  state?: string;
+  postal_code?: string;
+  country?: string;
+};
+
 export type PaymentType = "card" | "paymaya";
 export type SourceType = "gcash" | "grab_pay";
+export type RedirectType = {
+  success: string;
+  failed: string;
+  checkout_url?: string;
+};
+export type CurrencyType = "PHP";
