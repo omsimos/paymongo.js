103bc5ea99dd4da288d072d5f7fe241234d35beb
princejoogie <princejoogie@gmail.com>
2023-03-22 10:23:54 +0800

    chore: export payment method types

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export { createPaymongoClient } from "./client";
 export type { PaymentIntentOutput } from "./api/intent/types";
 export type { PaymentIntentCreateInput } from "./api/intent/create";
 export type { PaymentIntentRetrieveInput } from "./api/intent/retrieve";
+
+export type { PaymentMethodOutput } from "./api/method/types";
+export type { PaymentMethodCreateInput } from "./api/method/create";
