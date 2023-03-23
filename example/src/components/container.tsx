3709d23a55859a53d7de5045639bfb3812dbec64
princejoogie <princejoogie@gmail.com>
2023-03-23 10:07:36 +0800

    chore: add step in example

---
+++ example/src/components/container.tsx
@@ -0,0 +1,18 @@
+import { ReactNode, DetailedHTMLProps, HTMLAttributes } from "react";
+
+interface ContainerProps
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  children: ReactNode;
+}
+
+export const Container = ({
+  children,
+  className = "",
+  ...rest
+}: ContainerProps) => {
+  return (
+    <div {...rest} className={`mx-auto w-full max-w-4xl px-4 ${className}`}>
+      {children}
+    </div>
+  );
+};
--- example/src/env.mjs
+++ example/src/env.mjs
@@ -6,6 +6,7 @@ import { z } from "zod";
  */
 const server = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]),
+  PAYMONGO_SECRET_KEY: z.string().min(1),
 });
 
 /**
@@ -24,6 +25,7 @@ const client = z.object({
  */
 const processEnv = {
   NODE_ENV: process.env.NODE_ENV,
+  PAYMONGO_SECRET_KEY: process.env.PAYMONGO_SECRET_KEY,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
@@ -50,7 +52,7 @@ if (!!process.env.SKIP_ENV_VALIDATION == false) {
   if (parsed.success === false) {
     console.error(
       "❌ Invalid environment variables:",
-      parsed.error.flatten().fieldErrors,
+      parsed.error.flatten().fieldErrors
     );
     throw new Error("Invalid environment variables");
   }
@@ -64,7 +66,7 @@ if (!!process.env.SKIP_ENV_VALIDATION == false) {
         throw new Error(
           process.env.NODE_ENV === "production"
             ? "❌ Attempted to access a server-side environment variable on the client"
-            : `❌ Attempted to access server-side environment variable '${prop}' on the client`,
+            : `❌ Attempted to access server-side environment variable '${prop}' on the client`
         );
       return target[/** @type {keyof typeof target} */ (prop)];
     },
