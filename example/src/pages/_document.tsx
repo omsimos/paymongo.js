9e68a285344ebd620ca09f7b013da62604a8ecac
princejoogie <princejoogie@gmail.com>
2023-03-29 23:36:12 +0800

    chore: test example change

--- example/src/pages/_document.tsx
+++ example/src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <meta name="theme-color" content="#317EFB" />
+        <meta name="theme-color" content="#000000" />
 
         <title>paymongo.js</title>
         <meta
